74f4a9b9f6a417722941c210c735f65ff46ccb49
houjk <1805283656@qq.com>
2023-05-09 14:01:09 +0800

    modified:   src/router/routes.ts 	modified:   src/views/home.vue 	modified:   src/views/triangle.vue

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,16 +1,32 @@
 export const routes = [
-    {
-        path:'/',
-        name:'home',
-        component:()=>import('@/views/home.vue')
-    },
-    {
-        path:'/triangle',
-        name:'triangle',
-        component:()=>import('@/views/triangle.vue')
-    },{
-        path:'/VertexBuffer',
-        name:"VertexBuffer",
-        component:()=>import('@/views/VertexBuffer.vue')
-    }
-]
+  {
+    path: "/",
+    name: "home",
+    component: () => import("@/views/home.vue"),
+  },
+  {
+    path: "/triangle",
+    name: "triangle",
+    component: () => import("@/views/triangle.vue"),
+  },
+  {
+    path: "/VertexBuffer",
+    name: "VertexBuffer",
+    component: () => import("@/views/VertexBuffer.vue"),
+  },
+  {
+    path: "/VertexBuffer2",
+    name: "VertexBuffer2",
+    component: () => import("@/views/VertexBuffer2.vue"),
+  },
+  {
+    path: "/BufferBind",
+    name: "BufferBind",
+    component: () => import("@/views/BufferBind.vue"),
+  },
+  {
+    path:"/colorsTriangle",
+    name:"colorsTriangle",
+    component:()=>import("@/views/colorsTriangle.vue")
+},
+];
